75ec46b20ea11ee5b1df52d8011ade671f2a164c
augusto <augustocamaral2003@gmail.com>
2022-04-19 20:12:28 -0300

    fixed install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-cp .bashrc ~/
+cp .bashrc ~
 echo "Coppied .bashrc"
-cp .vimrc ~/
+cp .vimrc ~
 echo "Coppied .vimrc"
-cp .tmux.conf ~/
+cp .tmux.conf ~
 echo "Coppied .tmux.conf"
 cp scripts/* /usr/local/bin
 echo "Installed user scripts"
