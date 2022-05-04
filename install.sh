9f917213e7c3533da3d3eadb7ba6e076c4d3762b
augusto <augustocamaral2003@gmail.com>
2022-05-04 20:02:06 -0300

    modified install.sh

--- install.sh
+++ install.sh
@@ -19,3 +19,10 @@ else
             eval "$("$BASE16_SHELL/profile_helper.sh")"
     base16_isotope
 fi
+
+# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
+# so it won't get overriden
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+
+# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
+echo 'set completion-ignore-case On' >> ~/.inputrc
