b74d03e9684356971f2e111a468255484da0c412
augusto <augustocamaral2003@gmail.com>
2022-05-04 20:05:37 -0300

    updated install script

--- install.sh
+++ install.sh
@@ -20,9 +20,4 @@ else
     base16_isotope
 fi
 
-# If ~/.inputrc doesn't exist yet: First include the original /etc/inputrc
-# so it won't get overriden
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
-
-# Add shell-option to ~/.inputrc to enable case-insensitive tab completion
-echo 'set completion-ignore-case On' >> ~/.inputrc
+echo set completion-ignore-case on | sudo tee -a /etc/inputrc
